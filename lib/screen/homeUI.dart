964a41b3fd5bceb45fb7caec41d5a42eadadd32a
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-09 22:16:40 +0700

    Update homeUI.dart

--- lib/screen/homeUI.dart
+++ lib/screen/homeUI.dart
@@ -20,37 +20,31 @@ class _HomeUiState extends State<HomeUi> {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width * 0.1,
-                  ),
-                ),
                 Column(
                   children: [
-                    Text(
+                    const Text(
                       'Starts in 5 minutes',
                       style: TextStyle(
                         fontSize: 15,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.width * 0.1,
-                      ),
-                    ),
+                    const SizedBox(height: 5),
                     Container(
-                      padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Container(
+                            padding: EdgeInsets.all(10),
                             color: Colors.blue[300],
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
+                                Padding(padding: EdgeInsets.all(0.5)),
                                 Text(
                                   '09:45',
                                   style: TextStyle(
