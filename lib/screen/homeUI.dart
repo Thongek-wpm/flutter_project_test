ffcd78b8fadb95fbd1974efa2d00dd192459fe08
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-09 18:56:53 +0700

    Update homeUI.dart

--- lib/screen/homeUI.dart
+++ lib/screen/homeUI.dart
@@ -10,7 +10,30 @@ class HomeUi extends StatefulWidget {
 class _HomeUiState extends State<HomeUi> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(child: ,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.1,
+                  top: MediaQuery.of(context).size.width * 0.3,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Starts in 5 minutes',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
