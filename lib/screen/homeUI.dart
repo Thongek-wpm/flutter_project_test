8aeeffc851dd5fce8a7a9a6a7154919136fb42c8
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-09 23:10:42 +0700

    Update homeUI.dart

--- lib/screen/homeUI.dart
+++ lib/screen/homeUI.dart
@@ -13,114 +13,107 @@ class _HomeUiState extends State<HomeUi> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.only(
-                top: 15,
-              ),
-            ),
-            Row(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  children: [
-                    const Text(
-                      'Starts in 5 minutes',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                ),
+                Text(
+                  ' Starts in 5 minutes',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Container(
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: Container(
-                        clipBehavior: Clip.antiAlias,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              color: Colors.blue[500],
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.all(0.5),
-                                  ),
-                                  Text(
-                                    '09:45',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 15,
-                                    ),
-                                  ),
-                                  Padding(padding: EdgeInsets.only(bottom: 5)),
-                                  Text(
-                                    'Join',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          color: Colors.blue[500],
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(0.5),
                               ),
+                              Text(
+                                '09:45',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Padding(padding: EdgeInsets.only(bottom: 5)),
+                              Text(
+                                'Join',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(15),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Resume consutation',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
                             ),
-                            Padding(
-                              padding: EdgeInsets.all(15),
+                            SizedBox(
+                              height: 10,
                             ),
-                            Column(
+                            Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
+                                Icon(
+                                  Icons.access_time_sharp,
+                                  color: Colors.grey,
+                                  size: 15,
+                                ),
                                 Text(
-                                  'Resume consutation',
+                                  '30 min',
                                   style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15),
+                                    fontSize: 10,
+                                  ),
                                 ),
-                                SizedBox(
-                                  height: 10,
+                                Icon(
+                                  Icons.person_rounded,
+                                  size: 15,
+                                  color: Colors.grey,
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Icon(
-                                      Icons.access_time_sharp,
-                                      color: Colors.grey,
-                                      size: 15,
-                                    ),
-                                    Text(
-                                      '30 min',
-                                      style: TextStyle(
-                                        fontSize: 10,
-                                      ),
-                                    ),
-                                    Icon(
-                                      Icons.person_rounded,
-                                      size: 15,
-                                      color: Colors.grey,
-                                    ),
-                                    Text(
-                                      'John Fonter',
-                                      style: TextStyle(
-                                        fontSize: 10,
-                                      ),
-                                    ),
-                                  ],
+                                Text(
+                                  'Thongek Wpm',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                  ),
                                 ),
                               ],
                             ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 100),
-                            ),
-                            Icon(Icons.arrow_forward_ios),
                           ],
                         ),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 100),
+                        ),
+                        Icon(Icons.arrow_forward_ios),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
