f9ef5dc0af12b96850d33e92331f7c8850f9d241
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-09 20:35:24 +0700

    Update homeUI.dart

--- lib/screen/homeUI.dart
+++ lib/screen/homeUI.dart
@@ -12,31 +12,39 @@ class _HomeUiState extends State<HomeUi> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.1,
-                  top: MediaQuery.of(context).size.width * 0.3,
-                ),
-              ),
-              Row(
-                children: const [
-                  Text(
-                    'Starts in 5 minutes',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.1,
                   ),
-                  
-                ],
-              ),
-            ],
-          ),
-        ]),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Starts in 5 minutes',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.width * 0.1,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
