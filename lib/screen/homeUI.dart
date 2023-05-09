1b7d5eb95eeed578214f86ee202eb194cc2ce2b1
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-09 22:56:42 +0700

    Update homeUI.dart

--- lib/screen/homeUI.dart
+++ lib/screen/homeUI.dart
@@ -11,14 +11,17 @@ class _HomeUiState extends State<HomeUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.1,
+            Padding(
+              padding: EdgeInsets.only(
+                top: 15,
+              ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Column(
                   children: [
@@ -29,56 +32,92 @@ class _HomeUiState extends State<HomeUi> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    const SizedBox(height: 5),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            color: Colors.blue[300],
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Padding(padding: EdgeInsets.all(0.5)),
-                                Text(
-                                  '09:45',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              color: Colors.blue[500],
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.all(0.5),
                                   ),
-                                ),
+                                  Text(
+                                    '09:45',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(bottom: 5)),
+                                  Text(
+                                    'Join',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(15),
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
                                 Text(
-                                  'Join',
-                                  style: TextStyle(color: Colors.white),
+                                  'Resume consutation',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.access_time_sharp,
+                                      color: Colors.grey,
+                                      size: 15,
+                                    ),
+                                    Text(
+                                      '30 min',
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.person_rounded,
+                                      size: 15,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      'John Fonter',
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                'Resume consutation',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(Icons.access_time_sharp),
-                                  Text('30 min'),
-                                  Icon(Icons.person_rounded),
-                                  Text('John Fonter'),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Icon(Icons.arrow_forward_ios),
-                        ],
+                            Padding(
+                              padding: EdgeInsets.only(left: 100),
+                            ),
+                            Icon(Icons.arrow_forward_ios),
+                          ],
+                        ),
                       ),
                     ),
                   ],
