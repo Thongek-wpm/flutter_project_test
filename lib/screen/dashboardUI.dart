b954f8b97e9a419295dd736e74cfbd767d1de15e
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-10 16:05:48 +0700

    Update dashboardUI.dart

--- lib/screen/dashboardUI.dart
+++ lib/screen/dashboardUI.dart
@@ -16,12 +16,166 @@ class _DashboardUiState extends State<DashboardUi> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              'LIFETIME SERVICES BREAKDONW',
-              style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
+            padding: EdgeInsets.only(left: 15, right: 15),
+            child: Container(
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'LIFETIME SERVICES BREAKDONW',
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.circle,
+                                color: Colors.blue,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text(
+                                '35%',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text('Resume edits'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.circle,
+                                color: Colors.yellow,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text(
+                                '30%',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text('Interview prep'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.circle,
+                                color: Colors.pink,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text(
+                                '20%',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text('Education...'),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.circle,
+                                color: Colors.grey,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text(
+                                '15%',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5),
+                              ),
+                              Text('Other Services'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text(
+                              'Create invoice',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text(
+                              'View all invoices',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
