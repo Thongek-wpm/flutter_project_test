827770cec74ea6efd5815c4fcf20bd9504278304
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-11 16:35:22 +0700

    all swiper

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class ProfilsUi extends StatefulWidget {
   const ProfilsUi({super.key});
@@ -38,25 +40,28 @@ class _ProfilsUiState extends State<ProfilsUi> {
             ),
           ),
           Center(
-              child: CarouselSlider(
-            options: CarouselOptions(
-              height: 200,
-              enlargeCenterPage: true,
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return Image.network(
+                  imageUrls[index],
+                  fit: BoxFit.cover,
+                );
+              },
+              itemCount: imageUrls.length,
+              pagination: SwiperPagination(),
+              control: SwiperControl(),
             ),
-            items: imageList.map((imageList) {
-              return Image.network(imageList);
-            }).toList(),
-          ))
+          )
         ],
       ),
     );
   }
 }
 
-final List<String> imageList = [
-  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-  "https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
-  "https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg",
-  "https://cdn.pixabay.com/photo/2013/10/09/02/27/lake-192990__340.jpg",
-  "https://us.123rf.com/450wm/zagzig/zagzig1903/zagzig190300032/119363177-sunrise-at-the-old-boathouse-at-pooley-bridge-on-the-shores-of-ullswater-in-the-lake-district-in.jpg?ver=6"
+List<String> imageUrls = [
+  'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+  'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg',
+  'https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg',
+  'https://cdn.pixabay.com/photo/2013/10/09/02/27/lake-192990__340.jpg',
+  'https://us.123rf.com/450wm/zagzig/zagzig1903/zagzig190300032/119363177-sunrise-at-the-old-boathouse-at-pooley-bridge-on-the-shores-of-ullswater-in-the-lake-district-in.jpg?ver=6'
 ];
