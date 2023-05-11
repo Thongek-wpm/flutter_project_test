f6fb88e8c5e699a339ca06dba510c1f722ac83b1
Thongek Wogponom <thongek.wpm@gmail.com>
2023-05-11 16:16:23 +0700

    all fix

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class ProfilsUi extends StatefulWidget {
   const ProfilsUi({super.key});
@@ -35,9 +36,27 @@ class _ProfilsUiState extends State<ProfilsUi> {
                 ),
               ),
             ),
-          )
+          ),
+          Center(
+              child: CarouselSlider(
+            options: CarouselOptions(
+              height: 200,
+              enlargeCenterPage: true,
+            ),
+            items: imageList.map((imageList) {
+              return Image.network(imageList);
+            }).toList(),
+          ))
         ],
       ),
     );
   }
 }
+
+final List<String> imageList = [
+  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+  "https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+  "https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg",
+  "https://cdn.pixabay.com/photo/2013/10/09/02/27/lake-192990__340.jpg",
+  "https://us.123rf.com/450wm/zagzig/zagzig1903/zagzig190300032/119363177-sunrise-at-the-old-boathouse-at-pooley-bridge-on-the-shores-of-ullswater-in-the-lake-district-in.jpg?ver=6"
+];
